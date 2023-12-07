081916a3457c50cfc097a4fc44a5397baf82c63b
Ilya Dubkov <asdf@mail.ru>
2023-12-07 17:00:05 +0300

    add testing features general case

--- general_case/get_users_and_their_repositories.py
+++ general_case/get_users_and_their_repositories.py
@@ -1,32 +1,31 @@
 import asyncio
 import aiohttp
 import aiofiles
-import os
-import re
+import time
 
 from dotenv import load_dotenv
-from bs4 import BeautifulSoup
 from github import Github
 
 load_dotenv()
-g = Github(os.getenv("TOKEN"))
 
 async def get_urls_of_every_repos_on_page(url : str,query : str,pages : int):
     async with aiohttp.ClientSession() as session:
         for i in range(pages):
             async with session.get(url,params={"q": query,"page" : i}) as response:
+                print(response.status)
                 data = await response.json()
-                for repo in data['items']:
-                    print(repo['name'])
-                    print(repo['description'])
-                    print(repo['html_url'])
-                    print("-" * 50)
+                for repo in data["items"]:
+                    print(i,repo["owner"]["login"],repo["name"])
+                time.sleep(10)
 
 
 async def main():
+    PAGES_COUNT = 100
     async with asyncio.TaskGroup() as tg:
-        tg.create_task(get_urls_of_every_repos_on_page("https://api.github.com/search/repositories","Proof of concept",10))
-
+        tg.create_task(get_urls_of_every_repos_on_page("https://api.github.com/search/repositories","Proof of concept",PAGES_COUNT))
+        #https://api.github.com/search/repositories?q=Proof+of+concept&page=1
 
 if __name__ == "__main__":
+    start = time.time()
     asyncio.run(main())
+    print(time.time() - start)
