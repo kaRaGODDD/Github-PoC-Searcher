be1e0543b6cddf38db0d530e154268a099c6823f
kaRaGODDD <98781408+kaRaGODDD@users.noreply.github.com>
2023-12-16 15:08:23 +0300

    Update create_directories.py

--- general_case/create_directories.py
+++ general_case/create_directories.py
@@ -10,7 +10,6 @@ async def create_directory_by_user_name(name_of_the_directory : str):
     data_directory_name = os.getenv('NAME_OF_THE_DATA_DIRECTORY')
 
     if await asyncio.to_thread(os.path.exists, data_dir):
-        print(666)
         await asyncio.to_thread(os.chdir, data_dir)
         await asyncio.to_thread(os.makedirs, name_of_the_directory, exist_ok=True)
     else:
