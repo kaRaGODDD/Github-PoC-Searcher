89b1ce061df8b26578d7686589f7dd3bc73a6d2e
IlyaDubkov <asdf@mail.ru>
2023-12-24 22:23:43 +0300

    refactoring code and logic of that script

--- general_case/get_users_and_their_repositories.py
+++ general_case/get_users_and_their_repositories.py
@@ -1,23 +1,16 @@
 import asyncio
 import aiohttp
-import aiofiles
-import time
 import os
-import json
+import time
+import datetime
 
-from pydantic import BaseModel
 from dotenv import load_dotenv
-from github import Github
-from processing_repositories import processing_repository
+from formed_datetime_intervals import generate_intervals
 from math import ceil
-from datetime import datetime
-from typing import Optional
-
-from create_directories import create_directory_by_user_name
 
 load_dotenv()
 
-async def how_many_pages_need_to_parse(url : str):
+async def how_many_pages_need_to_parse(url: str):
     PER_SEARCH = 30
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as response:
@@ -25,28 +18,41 @@ async def how_many_pages_need_to_parse(url : str):
             count = data["total_count"]
     return ceil(count / PER_SEARCH)
 
+async def clone_repository(repository_url: str):
+    print(repository_url)
+    try:
+        process = await asyncio.create_subprocess_exec("git", "clone", repository_url + '.git')
+        await process.wait()
+    except Exception as e:
+        print(f"Error cloning repository {repository_url}: {e}")
+
+async def process_page(url: str, page: int):
+    token = os.getenv("GITHUB_TOKEN")
+    headers = {"Authorization": f"Bearer {token}"}
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url + f"&page={page}",headers=headers) as response:
+            data = await response.json()
+            print(url + f"&page={page}")
+            for item in data["items"]:
+                os.chdir(os.getenv("PATH_TO_THE_DATA_DIRECTORY"))
+                print(item["html_url"])
+                await clone_repository(item["html_url"])
+
 async def get_users_and_their_repositories():
-    #since and until just some constants 
-    since = datetime(2015,2,16)
-    until = datetime(2015,3,12)
-    url = await asyncio.to_thread(os.getenv,"URL")
-    url = url.format(since.strftime("%Y-%m-%d"),until.strftime("%Y-%m-%d"))
-    pages = await how_many_pages_need_to_parse(url)
-    for i in range(1,pages + 1):
-        async with aiohttp.ClientSession() as session:
-            async with session.get(url + f"&page={i}") as response:
-                data = await response.json()
-                print(url + f"&page={i}")
-                for item in data["items"]:
-                    await create_directory_by_user_name(item["owner"]["login"])
-                    await processing_repository(item["owner"]["login"],item["name"])
+    intervals = await generate_intervals(2015, 2016)
+    for interval in intervals:
+        since, until = interval
+        formatted_url = await asyncio.to_thread(os.getenv, "URL")
+        formatted_url = formatted_url.format(since, until)
+        pages = await how_many_pages_need_to_parse(formatted_url)
+        tasks = [process_page(formatted_url, i) for i in range(1, pages + 1)]
+        await asyncio.gather(*tasks)
 
 async def main():
     await get_users_and_their_repositories()
-     #TODO make array list with time data need which i need to parse
-     #https://api.github.com/search/repositories?q=Proof+of+concept&page=1
 
 if __name__ == "__main__":
     start = time.time()
     asyncio.run(main())
     print(time.time() - start)
+
