ce3b2aeee7dbd190955d31f24e0c7cd71ad64488
IlyaDubkov <asdf@mail.ru>
2023-12-30 19:33:42 +0300

    refactoring get user and their repositories

--- general_case/get_users_and_their_repositories.py
+++ general_case/get_users_and_their_repositories.py
@@ -1,12 +1,13 @@
+import subprocess
 import asyncio
 import aiohttp
 import os
 import time
-import datetime
 
 from dotenv import load_dotenv
 from formed_datetime_intervals import generate_intervals
 from math import ceil
+from create_directories import create_data_directory
 
 load_dotenv()
 
@@ -19,9 +20,8 @@ async def how_many_pages_need_to_parse(url: str):
     return ceil(count / PER_SEARCH)
 
 async def clone_repository(repository_url: str):
-    print(repository_url)
     try:
-        process = await asyncio.create_subprocess_exec("git", "clone", repository_url + '.git')
+        process = await asyncio.create_subprocess_exec("git", "clone", "--depth", "1", repository_url)
         await process.wait()
     except Exception as e:
         print(f"Error cloning repository {repository_url}: {e}")
@@ -30,29 +30,27 @@ async def process_page(url: str, page: int):
     token = os.getenv("GITHUB_TOKEN")
     headers = {"Authorization": f"Bearer {token}"}
     async with aiohttp.ClientSession() as session:
-        async with session.get(url + f"&page={page}",headers=headers) as response:
+        async with session.get(url + f"&page={page}&per_page={30}", headers=headers) as response:
             data = await response.json()
-            print(url + f"&page={page}")
+            await asyncio.to_thread(os.chdir, os.getenv("PATH_TO_THE_DATA_DIRECTORY"))
             for item in data["items"]:
-                os.chdir(os.getenv("PATH_TO_THE_DATA_DIRECTORY"))
-                print(item["html_url"])
-                await clone_repository(item["html_url"])
+                await clone_repository(item["clone_url"])
 
 async def get_users_and_their_repositories():
     intervals = await generate_intervals(2015, 2016)
-    for interval in intervals:
-        since, until = interval
+    for i in range (1,3):
+        since, until = intervals[i]
         formatted_url = await asyncio.to_thread(os.getenv, "URL")
         formatted_url = formatted_url.format(since, until)
         pages = await how_many_pages_need_to_parse(formatted_url)
-        tasks = [process_page(formatted_url, i) for i in range(1, pages + 1)]
+        tasks = [process_page(formatted_url, j) for j in range(1, pages + 1)]
         await asyncio.gather(*tasks)
 
 async def main():
+    await create_data_directory()
     await get_users_and_their_repositories()
 
 if __name__ == "__main__":
     start = time.time()
     asyncio.run(main())
     print(time.time() - start)
-
