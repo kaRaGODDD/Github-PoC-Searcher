b2b4286eb54daaa8f5964b16de5d942eba3da3bf
triplesss1@mail.ru <triplesss1@mail.ru>
2024-01-05 18:03:10 +0300

    add multiple git clone

--- general_case/get_users_and_their_repositories.py
+++ general_case/get_users_and_their_repositories.py
@@ -3,6 +3,7 @@ import asyncio
 import aiohttp
 import os
 import time
+import datetime
 
 from dotenv import load_dotenv
 from formed_datetime_intervals import generate_intervals
@@ -13,39 +14,56 @@ load_dotenv()
 
 async def how_many_pages_need_to_parse(url: str):
     PER_SEARCH = 30
+    print(url)
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as response:
             data = await response.json()
             count = data["total_count"]
     return ceil(count / PER_SEARCH)
 
-async def clone_repository(repository_url: str):
+async def clone_repository(repository_url: str, repository_name: str):
+    path_to_data_directory = os.getenv("PATH_TO_THE_DATA_DIRECTORY")
+    path_to_repository = os.path.join(path_to_data_directory, repository_name)
     try:
-        process = await asyncio.create_subprocess_exec("git", "clone", "--depth", "1", repository_url)
-        await process.wait()
+        exists = await asyncio.to_thread(os.path.exists, path_to_repository)
+        if exists:
+            print(f"Repository {repository_name} already clonned")
+        else:
+            process = await asyncio.create_subprocess_exec("git", "clone", "--depth", "1", repository_url, path_to_repository)
+            await process.wait()
     except Exception as e:
         print(f"Error cloning repository {repository_url}: {e}")
 
-async def process_page(url: str, page: int):
-    token = os.getenv("GITHUB_TOKEN")
-    headers = {"Authorization": f"Bearer {token}"}
+async def process_page(url: str, page: int,headers: dict):
     async with aiohttp.ClientSession() as session:
         async with session.get(url + f"&page={page}&per_page={30}", headers=headers) as response:
             data = await response.json()
-            await asyncio.to_thread(os.chdir, os.getenv("PATH_TO_THE_DATA_DIRECTORY"))
+            print(data)
             for item in data["items"]:
-                await clone_repository(item["clone_url"])
+                async with asyncio.TaskGroup() as tg:
+                    tg.create_task(clone_repository(item["clone_url"],item["name"]))
 
 async def get_users_and_their_repositories():
+    token = os.getenv("GITHUB_TOKEN")
+    headers = {"Authorization": f"Bearer {token}"}
+    first_year_for_scrapping = datetime.datetime(2015,1,1).strftime("%Y-%m-%d")
+    second_year_for_scrapping = datetime.datetime(2015,12,31).strftime("%Y-%m-%d")
+    url_for_scrapping_pages = os.getenv('URL') 
+    url_for_scrapping_pages = url_for_scrapping_pages.format(first_year_for_scrapping,second_year_for_scrapping) 
+    pages = await how_many_pages_need_to_parse(url_for_scrapping_pages)
     intervals = await generate_intervals(2015, 2016)
-    for i in range (1,3):
+    await asyncio.to_thread(os.chdir, os.getenv("PATH_TO_THE_DATA_DIRECTORY"))
+    for i in range(0, pages + 1):
         since, until = intervals[i]
         formatted_url = await asyncio.to_thread(os.getenv, "URL")
         formatted_url = formatted_url.format(since, until)
         pages = await how_many_pages_need_to_parse(formatted_url)
-        tasks = [process_page(formatted_url, j) for j in range(1, pages + 1)]
+        tasks = [process_page(formatted_url, j,headers) for j in range(0,pages+1)]
         await asyncio.gather(*tasks)
-
+        print(f"page#{i} done")
+        # async with asyncio.TaskGroup() as tg:
+        #     tg.create_task(process_page(formatted_url,i,headers))
+        
 async def main():
     await create_data_directory()
     await get_users_and_their_repositories()
