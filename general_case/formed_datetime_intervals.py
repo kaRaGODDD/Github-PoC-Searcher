4638dfa462895cdb9228d1039ccf33dcde8ba3a5
triplesss1@mail.ru <triplesss1@mail.ru>
2024-01-11 12:47:13 +0300

    improved the description of all functions

--- general_case/formed_datetime_intervals.py
+++ general_case/formed_datetime_intervals.py
@@ -1,17 +1,28 @@
 import datetime
-import asyncio
-
 from dateutil.relativedelta import relativedelta
 
-async def convert_datetime_object_to_ISO_8601(object_that_need_to_convert):
+async def convert_datetime_object_to_ISO_8601(datetime_object):
     '''
-        Function that convert datetime object to ISO 8601 standatrt 
+    Converts a datetime object to ISO 8601 standard.
+
+    Parameters:
+        - datetime_object: The datetime object to be converted.
+
+    Returns:
+        str: The ISO 8601 formatted string representation of the input datetime.
     '''
-    return datetime.datetime.strptime(object_that_need_to_convert, '%Y-%m-%d %H:%M:%S').isoformat() + "Z"
+    return datetime_object.strftime('%Y-%m-%dT%H:%M:%SZ')
 
 async def generate_intervals(first_year : int, second_year : int):
     '''
-        Function that generate the list of tuples, each tuple is the date from first year to second year per month
+    Generates a list of tuples, each tuple representing the start and end date for each month between the specified years.
+
+    Parameters:
+        - first_year (int): The starting year.
+        - second_year (int): The ending year.
+
+    Returns:
+        list: A list of tuples containing start and end dates for each month.
     '''
     intervals = [
         (
@@ -25,15 +36,20 @@ async def generate_intervals(first_year : int, second_year : int):
 
 async def from_one_interval_to_second_scrapping(first_interval : str, second_interval : str, flag : bool = False):
     '''
-        Function that splits intervals into months, if the number of repositories for the given queries > 1000
-        Splitting is done by months, for the given interval
+    Splits intervals into months if the number of repositories for the given queries is greater than 1000.
+    Splitting is done by months for the given interval.
+
+    Parameters:
+        - first_interval (str): The start date of the interval.
+        - second_interval (str): The end date of the interval.
+        - flag (bool): Flag indicating whether to split by months (default is False).
+
+    Returns:
+        list or tuple: A list of tuples containing start and end dates for each month, or the original interval.
     '''
     try:
         first_datetime_object = datetime.datetime.strptime(first_interval,"%Y-%m-%d")
         second_datetime_object = datetime.datetime.strptime(second_interval,"%Y-%m-%d")
-
-        if first_datetime_object.year != second_datetime_object.year or first_datetime_object.month != second_datetime_object.month:
-            raise Exception("Month and year should be the same")
 
         if first_datetime_object > second_datetime_object:
             raise Exception("First date should be earlier than the second one")
@@ -60,7 +76,14 @@ async def from_one_interval_to_second_scrapping(first_interval : str, second_int
 
 async def year_month_fixed_iterate_day_by_day(first_interval : str,second_interval : str):
     '''
-        Function that takes two string object both of them fix the same year and month, but function generate intervals through day
+    Takes two string objects that fix the same year and month, but the function generates intervals through days.
+
+    Parameters:
+        - first_interval (str): The start date of the interval.
+        - second_interval (str): The end date of the interval.
+
+    Returns:
+        list: A list of tuples containing start and end dates for each day between the specified intervals.
     '''
     try:
         first_datetime_object = datetime.datetime.strptime(first_interval,"%Y-%m-%d")   
