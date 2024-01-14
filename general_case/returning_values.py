b0481818eef80cc77aaeb3efcf23ca90f70f0c4f
kaRaGODDD <98781408+kaRaGODDD@users.noreply.github.com>
2024-01-14 15:20:35 +0300

    add returning URL from getenv

--- general_case/returning_values.py
+++ general_case/returning_values.py
@@ -7,7 +7,7 @@ async def return_token():
     return os.getenv("GITHUB_TOKEN")
 
 async def return_url():
-    return "https://api.github.com/search/repositories?q=proof%20of%20concept%20created:{}..{}"
+    return os.getenv("URL")
 
 async def return_last_date_scrapping():
     return os.getenv("LAST_DATE_SCRAPPING")
