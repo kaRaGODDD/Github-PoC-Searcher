23680dce05ec2d9cbafe97f555718a239bfb367c
triplesss1@mail.ru <triplesss1@mail.ru>
2024-01-14 16:08:30 +0300

    add working with api file

---
+++ general_case/working_with_API.py
@@ -0,0 +1,41 @@
+import aiohttp
+from math import ceil
+
+async def how_many_pages_need_to_parse(url: str, per_search: int):
+    '''
+    Async function that retrieves information from a given URL representing a search query
+    and calculates the number of pages needed to parse all the repositories based on the specified items per search.
+
+    Parameters:
+        - url (str): The URL representing a search query.
+        - per_search (int): The number of repositories/items to be retrieved per search.
+
+    Returns:
+        int: The calculated number of pages needed to parse all the repositories.
+    '''
+    if per_search > 100:
+        raise Exception("Per search parameter cannot be greater than 100")
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            data = await response.json()
+            count = data["total_count"]
+    return ceil(count / per_search)
+
+async def return_data_from_query(url: str, page: int,headers: dict, per_page: int):
+    '''
+    Async function that retrieves data from a query URL with pagination parameters.
+
+    Parameters:
+        - url (str): The base URL for the query.
+        - page (int): The page number for pagination.
+        - headers (dict): Headers to be included in the HTTP request.
+        - per_page (int): Number of items per page.
+
+    Returns:
+        dict: JSON data retrieved from the query.
+    '''
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url + f"&page={page}&per_page={per_page}", headers=headers) as response:
+            data = await response.json()
+    return data  
