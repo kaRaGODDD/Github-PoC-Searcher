f35d29a02bb1fe96650e89799b2bbbbca8a5165f
triplesss1@mail.ru <triplesss1@mail.ru>
2024-01-14 15:08:10 +0300

    delete some function from create directories

--- general_case/create_directories.py
+++ general_case/create_directories.py
@@ -18,24 +18,3 @@ async def create_data_directory():
     else:
         await asyncio.to_thread(os.chdir,os.getenv("PATH_TO_THE_BASE_DIR"))
         await asyncio.to_thread(os.makedirs,os.getenv("NAME_OF_THE_DATA_DIRECTORY"))
-
-async def create_directory_by_user_name(name_of_the_directory : str):
-    base_dir = os.getenv("PATH_TO_THE_BASE_DIR")
-    data_dir = os.getenv('PATH_TO_THE_DATA_DIRECTORY')
-    data_directory_name = os.getenv('NAME_OF_THE_DATA_DIRECTORY')
-
-    if await asyncio.to_thread(os.path.exists, data_dir):
-        await asyncio.to_thread(os.chdir, data_dir)
-        await asyncio.to_thread(os.makedirs, name_of_the_directory, exist_ok=True)
-    else:
-        await asyncio.to_thread(os.chdir, base_dir)
-        await asyncio.to_thread(os.makedirs, data_directory_name, exist_ok=True)
-        await asyncio.to_thread(os.chdir, data_dir)
-        await asyncio.to_thread(os.makedirs, name_of_the_directory, exist_ok=True)
-
-async def just_create_folder(folder_name : str,user_name : str):
-    path = os.getenv('PATH_TO_THE_DATA_DIRECTORY') + "/" + user_name
-    if await asyncio.to_thread(os.path.exists,path):
-        await asyncio.to_thread(os.chdir,path)
-        await asyncio.to_thread(os.makedirs,folder_name)
-
