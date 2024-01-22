94803a6281555f5d08b40010b68580cd98fabeae
triplesss1@mail.ru <triplesss1@mail.ru>
2024-01-22 23:07:28 +0300

    realize logic of handling data from request

--- scrapping_nvd_database/scrapping_nvd_database.py
+++ scrapping_nvd_database/scrapping_nvd_database.py
@@ -2,9 +2,12 @@ import asyncio
 import aiohttp
 import os
 
+from typing import List
 from dotenv import load_dotenv
+from pydantic import ValidationError
 from datetime_manager.create_datetime_intervals import create_intervals
 from constants_and_other_stuff.returning_values import return_nvd_api_url
+from constants_and_other_stuff.pydantic_models import CveExploit
 from constants_and_other_stuff.structs import StringInterval
 
 load_dotenv()
@@ -14,12 +17,25 @@ class NvdDataBaseScrapper:
         self.string_interval = string_interval
 
     async def start_scrapping(self):
-        ''''''
-        pass
+        await self._handle_intervals()
 
     async def _handle_api_request(self,url: str, string_interval: StringInterval):
         data = await self._return_data_from_request(url,string_interval)
-        '''TODO использовать pydantic для валидации! Перебор всех cve из data'''
+        await self._handle_data(data["vulnerabilities"])
+
+    async def _handle_intervals(self):
+        url = await return_nvd_api_url()
+        intervals = await create_intervals(self.string_interval)
+        tasks = [self._handle_api_request(url,interval) for interval in intervals]
+        await asyncio.gather(*tasks)
+
+    async def _handle_data(self,data: List[dict]):
+        for cve_info in data:
+            try:
+                cve_exploit = CveExploit(**cve_info['cve'])
+            except ValidationError as e:
+                print("Exception",e.json())
+            print(cve_exploit.id)
 
     async def _return_data_from_request(self, url: str, string_interval: StringInterval):
         async with aiohttp.ClientSession() as session:
@@ -27,14 +43,8 @@ class NvdDataBaseScrapper:
                 data = await response.json()
         return data
 
-    async def _handle_intervals(self):
-        url = await return_nvd_api_url()
-        intervals = await create_intervals(self.string_interval)
-        tasks = [self._handle_api_request(url,interval) for interval in intervals]
-        asyncio.gather(*tasks)
-
 async def main():
     ss = StringInterval("2015-03-01", "2015-03-06")
     asdf = NvdDataBaseScrapper(ss)
-    await asdf._handle_intervals()
+    await asdf.start_scrapping()
 asyncio.run(main())
