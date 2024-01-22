69cbe976e58b35e3936e9e0e8a7bbfb3195d6d07
triplesss1@mail.ru <triplesss1@mail.ru>
2024-01-22 20:25:32 +0300

    add pattern of md file

---
+++ constants_and_other_stuff/constants.py
@@ -0,0 +1,21 @@
+pattern_of_md_file = '''
+# ___[{}]({})___
+
+## Description
+
+- `{}`
+
+## Cvss data
+
+- `access vector`: {}
+- `base score`: {}
+- `exploitability score`: {}
+- `impact score`: {}
+- `base sevirity`: {}
+
+## Other information
+
+- `publish date`: {}
+- `vulnerability status`: {}
+- `references`: {}
+'''
