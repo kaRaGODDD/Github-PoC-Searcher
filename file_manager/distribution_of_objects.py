414aa8305f16bf3733826d7e9c7d1e492e5668bc
triplesss1@mail.ru <triplesss1@mail.ru>
2024-01-23 13:50:55 +0300

    added a formula for naming folders

--- file_manager/distribution_of_objects.py
+++ file_manager/distribution_of_objects.py
@@ -2,8 +2,10 @@ import aiofiles
 import os
 import re
 
+import math
 from dotenv import load_dotenv
 from constants_and_other_stuff.pydantic_models import CveExploit
+from constants_and_other_stuff.constants import pattern_of_md_file
 
 load_dotenv()
 
@@ -12,7 +14,14 @@ async def distribute_cve_object(cve_object: CveExploit):
     await _parse_cve_object_by_id(cve_object)
 
 async def _parse_cve_object_by_year(cve_object: CveExploit):
-    pass
+    pass    
 
 async def _parse_cve_object_by_id(cve_object: CveExploit):
-    pass
+    determine_folder_name_by_formula = await _determine_folder_name(cve_object.id)
+
+async def _determine_folder_name(cve_obj_id: str):
+    s = str(int(cve_obj_id.split("-")[2]) / 1000).split(".")
+    folder_name = s[0]
+    for i in range (0,len(s[1])):
+        folder_name += "x"
+    return folder_name
