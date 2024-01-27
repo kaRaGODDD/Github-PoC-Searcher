920dbe2f21bbda771542df78633326ecaeae4023
triplesss1@mail.ru <triplesss1@mail.ru>
2024-01-28 02:07:14 +0300

    add write file by poc

---
+++ file_manager/write_file_by_poc_pattern.py
@@ -0,0 +1,23 @@
+import os
+import aiofiles
+from dotenv import load_dotenv
+from constants_and_other_stuff.structs import CveModelForPoC, CvePoc
+from constants_and_other_stuff.constants import pattern_of_md_poc_file
+
+load_dotenv()
+
+async def write_poc(cve_id: str, poc_object: CveModelForPoC, path: str):
+    where_to_write = os.path.join(path, f"{cve_id}.md")
+    ready_poc_object = await _return_ready_poc_object(cve_id,poc_object)
+    
+    async with aiofiles.open(where_to_write, "w", encoding='utf-8') as f:
+        await f.write(pattern_of_md_poc_file.format(
+            ready_poc_object.cve_id,
+            ready_poc_object.description,
+            ready_poc_object.formatted_references
+        ))
+
+async def _return_ready_poc_object(cve_id: str, cve_model: CveModelForPoC) -> CvePoc:
+    description = cve_model.description[0].strip("[]'") if cve_model.description and cve_model.description[0] else ""
+    references_formatted = "\n".join([f'- ["Follow the link"]({ref})' for ref in cve_model.github_urls]) if cve_model.github_urls else ""
+    return CvePoc(cve_id, description, references_formatted)
