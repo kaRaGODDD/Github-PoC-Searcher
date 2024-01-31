ca19ed64d50a0629c32fd32e86e0ece8314ce342
triplesss1@mail.ru <triplesss1@mail.ru>
2024-01-31 04:07:48 +0300

    realize support of json and md writting

--- file_manager/distribution_of_objects.py
+++ file_manager/distribution_of_objects.py
@@ -6,24 +6,43 @@ import math
 from dotenv import load_dotenv
 from constants_and_other_stuff.structs import ComponentsOfCveID
 from constants_and_other_stuff.pydantic_models import CveExploit
-from file_manager.write_file import write_file_by_pattern
+from file_manager.write_file import write_file_by_pattern, write_file_by_json
 from create_directories.create_directories_on_pc import create_directory
+from constants_and_other_stuff.enums import FileSaving
+from typing import Optional
 
 load_dotenv()
 
-async def process_and_distribute_cve(cve_object: CveExploit):
+async def process_and_distribute_cve(cve_object: CveExploit,file_saving: FileSaving, json_answer: Optional[dict]={}):
     split_data = await _extract_cve_components(cve_object.id)
-    await _create_yearly_directories(split_data)
-    path = await _create_id_based_directory(split_data)
-    await write_file_by_pattern(cve_object, path)
+    match file_saving:
+        case FileSaving.MD:
+            await _create_yearly_directories(split_data,file_saving)
+            path = await _create_id_based_directory(split_data)
+            await write_file_by_pattern(cve_object, path)
+        case FileSaving.JSON:
+            await _create_yearly_directories(split_data,file_saving)
+            path = await _create_id_based_directory(split_data,file_saving)
+            await write_file_by_json(cve_object.id,json_answer,path)
 
-async def _create_yearly_directories(cve_object: ComponentsOfCveID):
-    await create_directory(os.getenv("NAME_OF_THE_DATA_DIRECTORY"), os.getenv("PATH_TO_THE_BASE_DIRECTORY"))
-    await create_directory(cve_object.cve_year, os.getenv("PATH_TO_THE_DATA_DIRECTORY"))
+async def _create_yearly_directories(cve_object: ComponentsOfCveID,file_saving: FileSaving):
+    match file_saving:
+        case FileSaving.MD:
+            await create_directory(os.getenv("NAME_OF_THE_DATA_DIRECTORY"), os.getenv("PATH_TO_THE_BASE_DIRECTORY"))
+            await create_directory(cve_object.cve_year, os.getenv("PATH_TO_THE_DATA_DIRECTORY"))
+        case FileSaving.JSON:
+            await create_directory(os.getenv("NAME_OF_THE_DATA_DIRECTORY"), os.getenv("PATH_TO_THE_BASE_DIRECTORY"))
+            await create_directory(os.getenv("NAME_OF_THE_JSON_AWSWERS_DIRECTORY"),os.getenv("PATH_TO_THE_DATA_DIRECTORY"))
+            await create_directory(cve_object.cve_year,os.getenv("PATH_TO_THE_JSON_ANSWERS_DIRECTORY"))
+            
 
-async def _create_id_based_directory(cve_object: ComponentsOfCveID) -> str:
+async def _create_id_based_directory(cve_object: ComponentsOfCveID,file_saving: FileSaving = FileSaving.MD) -> str:
     determine_folder_name_by_formula = await _generate_folder_name(cve_object)
-    full_path = os.path.join(os.getenv("PATH_TO_THE_DATA_DIRECTORY"), cve_object.cve_year)
+    match file_saving:
+        case FileSaving.MD:
+            full_path = os.path.join(os.getenv("PATH_TO_THE_DATA_DIRECTORY"), cve_object.cve_year)
+        case FileSaving.JSON:
+            full_path = os.path.join(os.getenv("PATH_TO_THE_JSON_ANSWERS_DIRECTORY"), cve_object.cve_year)
     await create_directory(determine_folder_name_by_formula, full_path)
     return os.path.join(full_path,determine_folder_name_by_formula)
 
