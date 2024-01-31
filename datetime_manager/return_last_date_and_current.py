edac41fc555a58f7350a7ff80c54c0adddf4fd54
triplesss1@mail.ru <triplesss1@mail.ru>
2024-01-31 17:22:11 +0300

    add return right datetime intervals for nvd update

--- datetime_manager/return_last_date_and_current.py
+++ datetime_manager/return_last_date_and_current.py
@@ -8,3 +8,6 @@ load_dotenv()
 
 async def return_last_current_intervals_for_poc_update():
     return StringInterval(os.getenv("LAST_DATE_SCRAPPING_POC_UPDATE"),datetime.now("%Y-%m-%d"))
+
+async def return_last_current_intervals_for_nvd_update():
+    return StringInterval(os.getenv("LAST_DATE_SCRAPPING_NVD_UPDATE"),datetime.now("%Y-%m-%d"))
