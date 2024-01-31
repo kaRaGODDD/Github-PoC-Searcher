484beb92b623cd7980adbc80bd009d0bab7c246d
triplesss1@mail.ru <triplesss1@mail.ru>
2024-01-31 04:06:22 +0300

    add solving file choising enum

--- constants_and_other_stuff/enums.py
+++ constants_and_other_stuff/enums.py
@@ -3,3 +3,8 @@ from enum import Enum, auto
 class POCChoiceSearch(Enum):
     GITHUB_API_SEARCH = auto()
     GRAPHQL_SEARCH = auto()
+
+
+class FileSaving(Enum):
+    JSON = auto()
+    MD = auto()
