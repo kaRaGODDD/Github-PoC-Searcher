5b73402de6d891c88429ade4bbd62815f8c8571e
triplesss1@mail.ru <triplesss1@mail.ru>
2024-02-01 02:16:46 +0300

    add function that realize search the cve object in different directories

--- file_manager/distribution_of_objects.py
+++ file_manager/distribution_of_objects.py
@@ -8,21 +8,44 @@ from constants_and_other_stuff.structs import ComponentsOfCveID
 from constants_and_other_stuff.pydantic_models import CveExploit
 from file_manager.write_file import write_file_by_pattern, write_file_by_json
 from create_directories.create_directories_on_pc import create_directory
-from constants_and_other_stuff.enums import FileSaving
+from constants_and_other_stuff.enums import FileSaving, TypeOfTheDirectory
 from typing import Optional
 
 load_dotenv()
 
+async def return_location_of_cve_object(string_cve_object: str, type_of_the_directory: TypeOfTheDirectory) -> Optional[str]:
+    data_about_cve = await _extract_cve_components(string_cve_object)  
+    match type_of_the_directory:
+        case TypeOfTheDirectory.PROOF_OF_CONCEPT_DIRECTORY:
+            path_determined_by_type_of_directory = await _get_directory_path_for_type(data_about_cve.cve_year, type_of_the_directory)
+            path_to_cve_object = await _get_directory_path_for_cve_object(data_about_cve, path_determined_by_type_of_directory)
+        case TypeOfTheDirectory.CVE_DATABASE_DIRECTORY:
+            path_determined_by_type_of_directory = await _get_directory_path_for_type(data_about_cve.cve_year, type_of_the_directory)
+            path_to_cve_object = await _get_directory_path_for_cve_object(data_about_cve, path_determined_by_type_of_directory)
+    return path_to_cve_object
+            
+    
+async def _get_directory_path_for_type(cve_year: int, type_of_the_directory: TypeOfTheDirectory) -> str:
+    match type_of_the_directory:
+        case TypeOfTheDirectory.PROOF_OF_CONCEPT_DIRECTORY:
+            return os.path.join(os.getenv("PATH_TO_THE_DATA_DIRECTORY"), cve_year)
+        case TypeOfTheDirectory.CVE_DATABASE_DIRECTORY:
+            return os.path.join(os.getenv("PATH_TO_THE_POC_DIRECTORY"),cve_year)
+
+async def _get_directory_path_for_cve_object(cve_object:  ComponentsOfCveID, path_to_cve_object: str) -> str:
+    determine_folder_name_by_formula = await _generate_folder_name(cve_object)
+    return os.path.join(path_to_cve_object, determine_folder_name_by_formula)
+
 async def process_and_distribute_cve(cve_object: CveExploit,file_saving: FileSaving, json_answer: Optional[dict]={}):
-    split_data = await _extract_cve_components(cve_object.id)
+    components_of_the_cve = await _extract_cve_components(cve_object.id)
     match file_saving:
         case FileSaving.MD:
-            await _create_yearly_directories(split_data,file_saving)
-            path = await _create_id_based_directory(split_data)
+            await _create_yearly_directories(components_of_the_cve,file_saving)
+            path = await _create_id_based_directory(components_of_the_cve)
             await write_file_by_pattern(cve_object, path)
         case FileSaving.JSON:
-            await _create_yearly_directories(split_data,file_saving)
-            path = await _create_id_based_directory(split_data,file_saving)
+            await _create_yearly_directories(components_of_the_cve,file_saving) #Вот здесь вот получить пути и создать необходимую директорию
+            path = await _create_id_based_directory(components_of_the_cve,file_saving)
             await write_file_by_json(cve_object.id,json_answer,path)
 
 async def _create_yearly_directories(cve_object: ComponentsOfCveID,file_saving: FileSaving):
