c089e3635c17b07dbdf45e026f4442ebc8e9e794
triplesss1@mail.ru <triplesss1@mail.ru>
2024-02-01 21:44:56 +0300

    realize check the path

--- file_manager/read_files.py
+++ file_manager/read_files.py
@@ -5,9 +5,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 async def read_file_by_path(cve_object_path: str):
-    try:
-        async with aiofiles.open(cve_object_path, "r", encoding='utf-8') as f:
-            content = await f.read()
-            return content
-    except Exception as e:
-        print(f"Error was occured: {e}")
+    if os.path.exists(cve_object_path):
+        try:
+            async with aiofiles.open(cve_object_path, "r", encoding='utf-8') as f:
+                content = await f.read()
+                return content
+        except Exception as e:
+            print(f"Error was occured: {e}")
+    else:
+        print(f"That path doesnt exists {cve_object_path}")
