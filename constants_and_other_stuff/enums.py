c394eb5f0c1caa15267ffa74afce538989effe4b
triplesss1@mail.ru <triplesss1@mail.ru>
2024-02-03 00:08:13 +0300

    add new enum which responsible for choising type of find pocs

--- constants_and_other_stuff/enums.py
+++ constants_and_other_stuff/enums.py
@@ -9,6 +9,16 @@ class FileSaving(Enum):
     JSON = auto()
     MD = auto()
 
+
 class TypeOfTheDirectory(Enum):
     PROOF_OF_CONCEPT_DIRECTORY = auto()
     CVE_DATABASE_DIRECTORY = auto()
+
+
+class TypeOfScrapping(Enum):
+    UPDATE = auto()
+    SCRAPPING = auto()
+
+class TypeOfPOCSearching(Enum):
+    TRAVERSE_ALL_CVE_DIRECTORY = auto()
+    TRAVERSE_FIX_YEAR_ON_CVE_DIRECTORY = auto()
