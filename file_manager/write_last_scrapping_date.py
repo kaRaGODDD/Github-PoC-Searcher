7c12282963b3a81d0ef2fac9e689e3e54533ef25
triplesss1@mail.ru <triplesss1@mail.ru>
2024-02-02 20:07:07 +0300

    realize logic of format the last date and name of the key in parameters of function

--- file_manager/write_last_scrapping_date.py
+++ file_manager/write_last_scrapping_date.py
@@ -3,9 +3,9 @@ from dotenv import load_dotenv, set_key
 
 load_dotenv()
 
-async def write_last_date_scrapping():
+async def write_last_date_scrapping(name_of_the_key: str, datetime_interval_format: str="%Y-%m-%d"):
     try:
-        set_key(".env", "LAST_DATE_SCRAPPING", datetime.datetime.now().strftime("%Y-%m-%d"))
+        set_key(".env", name_of_the_key, datetime.datetime.now().strftime(datetime_interval_format))
     except Exception as e:
         e.add_note("Cannot set key in function write last date scrapping")
     
