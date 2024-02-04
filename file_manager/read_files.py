5be22241cd86c4653dfc35f8abb045a1118f5a87
triplesss1@mail.ru <triplesss1@mail.ru>
2024-02-04 19:25:00 +0300

    add description

--- file_manager/read_files.py
+++ file_manager/read_files.py
@@ -7,7 +7,8 @@ from constants_and_other_stuff.enums import TypeOfTheDirectory
 load_dotenv()
 
 async def read_file_by_path(cve_object_path: str, type_of_the_directory: Optional[TypeOfTheDirectory] = None) -> Union[str, None]:
-    if not os.path.exists(cve_object_path) and type_of_the_directory:
+    '''Return data from specified path'''
+    if not os.path.exists(cve_object_path):
         print(f"That path doesn't exist: {cve_object_path}")
         return None
     try:
