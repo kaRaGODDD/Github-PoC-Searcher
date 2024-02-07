d60dd8937f9171630fceebd733e2a90d4eaa643f
triplesss1@mail.ru <triplesss1@mail.ru>
2024-02-07 23:13:26 +0300

    update

--- constants_and_other_stuff/enums.py
+++ constants_and_other_stuff/enums.py
@@ -1,5 +1,6 @@
 from enum import Enum, auto
 
+
 class POCSearchMethod(Enum):
     GITHUB_API_SEARCH = auto()
     GRAPHQL_SEARCH = auto()
@@ -11,16 +12,17 @@ class FileFormat(Enum):
 
 
 class DirectoryType(Enum):
-    PROOF_OF_CONCEPT_DIRECTORY = auto()
+    POC_DIRECTORY = auto()
     CVE_DATABASE_DIRECTORY = auto()
 
 
 class ScrapingType(Enum):
     UPDATE = auto()
-    SCRAPPING = auto()
+    SCRAPING = auto()
 
 
 class POCSearchType(Enum):
-    TRAVERSE_ALL_CVE_DIRECTORIES = auto()
+    TRAVERSE_ALL_CVE_DIRECTORIES_CONSISTENTLY = auto()
     TRAVERSE_FIX_YEAR_ON_CVE_DIRECTORY = auto()
-    TRAVERSE_MULTIPLE_DIRECTORIES = auto()
+    TRAVERSE_ALL_DIRECTORIES_AT_ONCE = auto()
+    
