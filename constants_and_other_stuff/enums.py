9231986b4c2840770ddf15789032e667acea29c6
triplesss1@mail.ru <triplesss1@mail.ru>
2024-02-07 12:19:44 +0300

    refactor naming of enums

--- constants_and_other_stuff/enums.py
+++ constants_and_other_stuff/enums.py
@@ -1,24 +1,26 @@
 from enum import Enum, auto
 
-class POCChoiceSearch(Enum):
+class POCSearchMethod(Enum):
     GITHUB_API_SEARCH = auto()
     GRAPHQL_SEARCH = auto()
 
 
-class FileSaving(Enum):
+class FileFormat(Enum):
     JSON = auto()
     MD = auto()
 
 
-class TypeOfTheDirectory(Enum):
+class DirectoryType(Enum):
     PROOF_OF_CONCEPT_DIRECTORY = auto()
     CVE_DATABASE_DIRECTORY = auto()
 
 
-class TypeOfScrapping(Enum):
+class ScrapingType(Enum):
     UPDATE = auto()
     SCRAPPING = auto()
 
-class TypeOfPOCSearching(Enum):
-    TRAVERSE_ALL_CVE_DIRECTORY = auto()
+
+class POCSearchType(Enum):
+    TRAVERSE_ALL_CVE_DIRECTORIES = auto()
     TRAVERSE_FIX_YEAR_ON_CVE_DIRECTORY = auto()
+    TRAVERSE_MULTIPLE_DIRECTORIES = auto()
