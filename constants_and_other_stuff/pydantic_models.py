f8bd75208d4fe2ab4d91040904eff12c717f0daf
triplesss1@mail.ru <triplesss1@mail.ru>
2024-02-09 17:37:23 +0300

    add new pydantic model named fast search validator for new method of searching pocs based on datetime intervals

--- constants_and_other_stuff/pydantic_models.py
+++ constants_and_other_stuff/pydantic_models.py
@@ -5,18 +5,22 @@ from typing import List, Optional
 class _Item(BaseModel):
     html_url: str
 
-class HtmlUrlFromResponse(BaseModel):
+
+class UrlFromResponse(BaseModel):
     total_count: int
     items: Optional[List[_Item]] = Field(None,alias='items')
+
 
 class _Source(BaseModel):
     url: str
     source: str
     tags: Optional[List[str]] = Field(None, alias='tags')
 
+
 class _CvssData(BaseModel):
     accessVector: str
     baseScore: float
+
 
 class _Score(BaseModel):
     exploitabilityScore: float
@@ -25,12 +29,15 @@ class _Score(BaseModel):
     source: str
     cvssData: _CvssData
 
+
 class _Description(BaseModel):
     lang: str
     value: str
 
+
 class _Metrics(BaseModel):
     cvssMetricV2: Optional[List[_Score]] = []
+
 
 class CveExploit(BaseModel):
     id: str
@@ -39,19 +46,63 @@ class CveExploit(BaseModel):
     descriptions: List[_Description]
     references: List[_Source]
     metrics: _Metrics
-    
+
+
 class _RepositoryQL(BaseModel):
     url: str
 
+
 class _EdgeQL(BaseModel):
     node: _RepositoryQL
+
 
 class _SearchQL(BaseModel):
     repositoryCount: int
     edges: List[_EdgeQL]
 
+
 class _DataQL(BaseModel):
     search: _SearchQL
 
+
 class GraphQLAnswerModel(BaseModel):
     data: _DataQL
+
+
+class _Topic(BaseModel):
+    name: Optional[str] = Field("", alias='name')
+
+
+class _NodeTopic(BaseModel):
+    topic: _Topic
+
+
+class _Edge(BaseModel):
+    node: _NodeTopic
+
+
+class _RepositoryTopics(BaseModel):
+    edges: Optional[List[_Edge]] = Field([], alias='edges')
+
+
+class _Node(BaseModel):
+    name: str
+    description: Optional[str] = Field("", alias='description')
+    repositoryTopics: _RepositoryTopics
+
+
+class EdgeData(BaseModel):
+    node: _Node
+
+
+class _Search(BaseModel):
+    repositoryCount: int
+    edges: Optional[List[EdgeData]] = Field([], alias='edges')
+
+
+class _Data(BaseModel):
+    search: _Search
+
+
+class FastSearchValidator(BaseModel):
+    data: _Data
