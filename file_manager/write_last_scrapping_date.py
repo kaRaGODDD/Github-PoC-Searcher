0d947bd3917f58468856668f07cade14e8eab074
triplesss1@mail.ru <triplesss1@mail.ru>
2024-02-09 21:11:58 +0300

    refactoring all imports and from ...

--- file_manager/write_last_scrapping_date.py
+++ file_manager/write_last_scrapping_date.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv, set_key
 
 load_dotenv()
 
-async def write_last_date_scrapping(name_of_the_key: str, datetime_interval_format: str="%Y-%m-%d"):
+async def write_last_date_scraping(name_of_the_key: str, datetime_interval_format: str="%Y-%m-%d"):
     try:
         set_key(".env", name_of_the_key, datetime.datetime.now().strftime(datetime_interval_format))
     except Exception as e:
