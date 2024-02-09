d5122c7e019e8ae06939b1083d61c1d6c7299a2d
triplesss1@mail.ru <triplesss1@mail.ru>
2024-02-09 21:10:47 +0300

    refactoring that class

--- github_manager/personal_github_manager.py
+++ github_manager/personal_github_manager.py
@@ -1,9 +1,12 @@
 import os
+
 from abc import ABC, abstractmethod
-from github import Github
-from git import Repo
 from dotenv import load_dotenv
+
+from github import Github
+
 from constants_and_other_stuff.returning_values import return_github_token
+
 
 load_dotenv()
 
