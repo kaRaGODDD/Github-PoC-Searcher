f492c1d2d00c3d3fad365109105b799dc9855667
triplesss1@mail.ru <triplesss1@mail.ru>
2024-02-24 15:25:20 +0300

    with help of source comes choice

--- datetime_manager/create_datetime_intervals.py
+++ datetime_manager/create_datetime_intervals.py
@@ -6,20 +6,25 @@ from dotenv import load_dotenv
 from dateutil.relativedelta import relativedelta
 
 from constants_and_other_stuff.structs import StringInterval, DatetimeInterval
+from constants_and_other_stuff.enums import Sources
 
 
 load_dotenv()
 
-async def create_intervals(interval: StringInterval) -> List[StringInterval]:
-    intervals = await _split_interval_by_parts(interval)
+async def create_intervals(interval: StringInterval, source: Sources = Sources.GITHUB) -> List[StringInterval]:
+    intervals = await _split_interval_by_parts(interval, source)
     return intervals
 
-async def _split_interval_by_parts(interval: StringInterval) -> List[StringInterval]:
+async def _split_interval_by_parts(interval: StringInterval, source: Sources) -> List[StringInterval]:
     new_interval = await _convert_to_datetime_interval(interval)
     current_date, end_date = await _return_components(new_interval)
     dates = []
     while current_date < end_date:
-        next_month = min(current_date + relativedelta(days=1),end_date)
+        match source:
+            case Sources.GITHUB:
+                next_month = min(current_date + relativedelta(days=1), end_date)
+            case Sources.NVD:
+                next_month = min(current_date + relativedelta(month=1), end_date)
         string_interval_object = StringInterval(current_date.strftime("%Y-%m-%d"),next_month.strftime("%Y-%m-%d"))
         dates.append(string_interval_object)
         current_date = next_month
